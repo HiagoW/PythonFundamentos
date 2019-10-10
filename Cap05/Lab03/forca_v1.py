7c2848cb23f38a10bc481d751cbe95cb70907513
Hiago W Petris <hiago.petris@gmail.com>
2019-10-10 13:16:27 -0300

    Basico

--- Cap05/Lab03/forca_v1.py
+++ Cap05/Lab03/forca_v1.py
@@ -71,27 +71,52 @@ class Hangman:
 
 	# Método Construtor
 	def __init__(self, word):
-		
+		self.word = word
+		self.i = 0
+		self.right = list()
+		self.wrong = list()
+		self.hide_word()
 		
 	# Método para adivinhar a letra
 	def guess(self, letter):
-		
+		if (letter in self.right or letter in self.wrong):
+			print("Voce já digitou essa letra")
+			return
+		elif letter in self.word:
+			print("Você acertou!")
+			self.right.append(letter)
+			self.show = list(map(lambda x: x if x in self.right else '_', self.word))
+		else:
+			print("Você errou!")
+			self.wrong.append(letter)
+			self.i += 1
 		
 	# Método para verificar se o jogo terminou
 	def hangman_over(self):
-		
+		if self.i == len(board)-1 or self.hangman_won():
+			return True
+		self.hangman_won()
 		
 	# Método para verificar se o jogador venceu
 	def hangman_won(self):
-		
+		if self.show == list(self.word):
+			return True
+		return False
 
 	# Método para não mostrar a letra no board
 	def hide_word(self):
+		self.show = len(self.word) * '_'
+		return
 		
 		
 	# Método para checar o status do game e imprimir o board na tela
 	def print_game_status(self):
-		
+		print(board[self.i])
+		for i in self.show:
+			print(i, end='')
+		print()
+		print("Certas: " + self.right.__str__())
+		print("Erradas: " + self.wrong.__str__())
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
@@ -107,10 +132,13 @@ def main():
 	game = Hangman(rand_word())
 
 	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
+	while(not game.hangman_over()):
+		game.print_game_status()
+		entrada = input("Digite uma letra: ")
+		game.guess(entrada)
 
 	# Verifica o status do jogo
-	game.print_game_status()	
+	game.print_game_status()
 
 	# De acordo com o status, imprime mensagem na tela para o usuário
 	if game.hangman_won():
